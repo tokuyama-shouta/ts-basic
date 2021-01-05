6a4ccd147bbaa1fb7ae25d1624cbed1e88f9dd99
tokuyama shouta <toku1031ts@gmail.com>
2021-01-05 16:01:22 +0900

    基本の型定義

--- src/world.ts
+++ src/world.ts
@@ -11,3 +11,15 @@ export default class World {
     }
   }
 }
+
+// プリミティブ型
+// string: 文字列
+// number: 数値
+// boolean: trueとfalseの2つの値を扱う型
+
+const name = 'pkpk'
+const age = 28
+const isSingle = true
+
+// 判定式の結果も代入できる
+const isOver20: boolean = age >= 20
