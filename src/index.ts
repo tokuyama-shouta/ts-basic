1f5b224b76efcf3820ec3742b969a478a3d3c3dd
tokuyama shouta <toku1031ts@gmail.com>
2021-01-07 21:27:49 +0900

    関数のパラメーターと戻り値に型をつける

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,19 @@
 // world.sayHello(root)
 
 // 03 基本の型定義
-import { primitiveSample, notExistSample, anySample, unknownSample } from './basic'
+// import { primitiveSample, notExistSample, anySample, unknownSample } from './basic'
 
-primitiveSample()
-notExistSample()
-anySample()
-unknownSample()
+// primitiveSample()
+// notExistSample()
+// anySample()
+// unknownSample()
+
+// 04.関数の型定義
+import { logMessage } from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+logMessage('Hello TypeScript!')
+isUserSignedIn('ABC', 'pkpk')
+isUserSignedIn('DEF')
+isUserSignedIn2('ABC')
+const sum = sumProductsPrice(500, 200, 300, 400, 500)
+console.log('Function parameters sample 5:', sum)
