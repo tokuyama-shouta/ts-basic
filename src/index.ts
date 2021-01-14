29caec6c19c23359f68a31d80e69721d0f132936
tokuyama shouta <toku1031ts@gmail.com>
2021-01-14 20:22:59 +0900

    配列・タプルの型定義

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,13 @@
 // console.log('Function parameters sample 5:', sum)
 
 // 05.オブジェクトの型定義
-import objectSample from './object/object'
-import typeAliasSample from './object/ailias'
-objectSample()
-typeAliasSample()
+// import objectSample from './object/object'
+// import typeAliasSample from './object/ailias'
+// objectSample()
+// typeAliasSample()
+
+// 06.配列とダブルの型定義
+import arraySample from './array/array'
+import tupleSample from './array/tuple'
+arraySample()
+tupleSample()
--- src/object/object.ts
+++ src/object/object.ts
@@ -4,50 +4,42 @@ export default function objectSample() {
   //   age: 28
   // }
   // a.name
-
   //オブジェクトリテラル記法で型定義
-  let country: {
-    language: string
-    name: string
-  } = {
-    language: 'Japanese',
-    name: 'Japan',
-  }
-
-  console.log('Object object sample 1:', country)
-
-  country = {
-    language: 'English',
-    name: 'United States of America',
-  }
-
-  console.log('Object object sample 2:', country)
-
+  // let country: {
+  //   language: string
+  //   name: string
+  // } = {
+  //   language: 'Japanese',
+  //   name: 'Japan',
+  // }
+  // console.log('Object object sample 1:', country)
+  // country = {
+  //   language: 'English',
+  //   name: 'United States of America',
+  // }
+  // console.log('Object object sample 2:', country)
   // オプショナルとreadonly
-  const pkpk: {
-    age: number
-    lastName: string
-    readonly firstName: string
-    gender?: string
-  } = {
-    age: 28,
-    lastName: 'Yamada',
-    firstName: 'Tarou',
-  }
-
-  pkpk.gender = 'male'
-  pkpk.lastName = 'kamado'
-  console.log('Object object sample 3:', pkpk)
-
+  // const pkpk: {
+  //   age: number
+  //   lastName: string
+  //   readonly firstName: string
+  //   gender?: string
+  // } = {
+  //   age: 28,
+  //   lastName: 'Yamada',
+  //   firstName: 'Tarou',
+  // }
+  // pkpk.gender = 'male'
+  // pkpk.lastName = 'kamado'
+  // console.log('Object object sample 3:', pkpk)
   // インデックスシグネチャ
-  const capitals: {
-    [countryName: string]: string
-  } = {
-    Japan: 'Tokyo',
-    Korea: 'seoul',
-  }
-
-  capitals.China = 'Beijing'
-  capitals.Canada = 'Ottawa'
-  console.log('Object object sample 4:', capitals)
+  // const capitals: {
+  //   [countryName: string]: string
+  // } = {
+  //   Japan: 'Tokyo',
+  //   Korea: 'seoul',
+  // }
+  // capitals.China = 'Beijing'
+  // capitals.Canada = 'Ottawa'
+  // console.log('Object object sample 4:', capitals)
 }
