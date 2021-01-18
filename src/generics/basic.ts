fe56a44fe15180cb168ee65a45c725ac67774535
tokuyama shouta <toku1031ts@gmail.com>
2021-01-18 16:17:32 +0900

    ジェネリクス型

---
+++ src/generics/basic.ts
@@ -0,0 +1,57 @@
+export default function genericsBasicSample() {
+  //ジェネリック型を使わない場合
+  const stringReduce = (array: string[], initialValue: string): string => {
+    let result = initialValue
+    for (let i = 0; i < array.length; i++) {
+      result += array[i]
+    }
+    return result
+  }
+  console.log('Generics basic sample 1:', stringReduce(['May ', 'the ', 'force ', 'be ', 'with ', 'you'], ''))
+
+  const numberReduce = (array: number[], initialValue: number): number => {
+    let result = initialValue
+    for (let i = 0; i < array.length; i++) {
+      result += array[i]
+    }
+    return result
+  }
+  console.log('Generics basic sample 2:', numberReduce([100, 200, 300], 1000))
+
+  type Reduce = {
+    (array: string[], initialValue: string): string
+    (array: number[], initialValue: number): number
+  }
+
+  type GenericReduce<T> = {
+    (array: T[], initialValue: T): T
+  }
+  const genericStringReduce: GenericReduce<string> = (array, initialValue) => {
+    let result = initialValue
+    for (let i = 0; i < array.length; i++) {
+      result += array[i]
+    }
+    return result
+  }
+  console.log('Generics basic sample 3:', genericStringReduce(['MAKE ', 'TYPESCRIPT ', 'GREAT '], 'AGAIN'))
+
+  const genericNumberReduce: GenericReduce<number> = (array, initialValue) => {
+    let result = initialValue
+    for (let i = 0; i < array.length; i++) {
+      result += array[i]
+    }
+    return result
+  }
+  console.log('Generics basic sample 4:', genericNumberReduce([-100, -200, -300], 1000))
+
+  //いろいろなジェネリクス型の定義方法
+  //完全な呼び出しシグネチャ（ここのしっグネちゃにジェネリクス型を割り当てる)
+  type GemnericReduce2 = {
+    <T>(array: T[], initialValue: T): T
+    <U>(array: U[], initialValue: U): U
+  }
+
+  //呼び出しシグネチャの省略記法
+  type GenericReduce3<T> = (array: T[], initialValue: T) => T
+  type GenericReduce4<U> = (array: U[], initialValue: U) => U
+}
--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,13 @@
 // typeAliasSample()
 
 // 06.配列とダブルの型定義
-import arraySample from './array/array'
-import tupleSample from './array/tuple'
-arraySample()
-tupleSample()
+// import arraySample from './array/array'
+// import tupleSample from './array/tuple'
+// arraySample()
+// tupleSample()
+
+// 07.ジェネリック型とポリモーフィズム
+import genericsBasicSample from './generics/basic'
+import genericsAdvancedSample from './generics/advanced'
+genericsBasicSample()
+genericsAdvancedSample()
